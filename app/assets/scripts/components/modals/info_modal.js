de0ed022d0f8e7a32107b2916a7aa04e03cd4a4c
Ricardo Mestre <ricardo@duplos.org>
2015-05-25 16:26:55 +0100

    Change info modal copy. Contribute to #18

--- app/assets/scripts/components/modals/info_modal.js
+++ app/assets/scripts/components/modals/info_modal.js
@@ -6,15 +6,24 @@ var BModal = require('./base_modal');
 
 var InfoModal = React.createClass({
   getHeader: function() {
-    return (<h1 className="modal-title">Modal title</h1>);
+    return (<h1 className="modal-title">About</h1>);
   },
 
   getBody: function() {
-    return (<p>Modal body</p>);
+    return (
+      <div className="prose">
+        <h2>What</h2>
+        <p>OpenAerialMap (OAM) is a collection of openly licensed satellite and unmanned aerial vehicle (UAV) imagery. OAM is an open serivce that will provide storage and search of this imagery.</p>
+        <h2>Why</h2>
+        <p>Imagery is being generated more and more, however after a disaster it is difficult to determine what imagery is available and where to access it. OAM seeks to solve this by providing a simple, open way to process and provide imagery for humanitarian response and disaster preparedness.</p>
+        <h2>Join</h2>
+        <p>There are plenty of ways to get involved in OpenAerialMap. Check out the <a href="https://github.com/hotosm/OpenAerialMap" title="Visit the GitHub repository">GitHub repository</a> to learn more about the design and how to get involved in the project.</p>
+      </div>
+    );
   },
 
   getFooter: function() {
-    return (<p>Modal footer</p>);
+    return (null);
   },
 
   render: function () {

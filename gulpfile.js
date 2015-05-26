980c04208fbc8172dfd7ba931a5334a7a16df767
Daniel Silva <daniel@developmentseed.org>
2015-05-26 11:36:41 +0100

    Contribute to #2. Initial travis setup.
    Add new tasks and overall gulpfile improvement.

--- gulpfile.js
+++ gulpfile.js
@@ -11,39 +11,141 @@ var buffer = require('vinyl-buffer');
 var gutil = require('gulp-util');
 var sourcemaps = require('gulp-sourcemaps');
 var assign = require('lodash').assign;
+var clean = require('gulp-clean');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
 
 
-// From: https://github.com/gulpjs/gulp/blob/master/docs/recipes/fast-browserify-builds-with-watchify.md
-// Add custom browserify options here
-var customOpts = {
-  entries: ['./app/assets/scripts/main.js'],
-  debug: true
-};
-var opts = assign({}, watchify.args, customOpts);
-var b = watchify(browserify(opts)); 
-// Add transformations here
-b.transform(reactify);
+////////////////////////////////////////////////////////////////////////////////
+//--------------------------- Variables --------------------------------------//
+//----------------------------------------------------------------------------//
 
-gulp.task('scripts:build', bundle); // so you can run `gulp js` to build the file
-b.on('update', bundle); // on any dep update, runs the bundler
-b.on('log', gutil.log); // output build logs to terminal
+// Control whether the browser should reload.
+var shouldReload = true;
+// Control weather we're watching for changes.
+// Basically means the watch task was called.
+var shouldWatch = false;
+// Environment for the script.
+var ENV = 'development';
 
-function bundle() {
-  return b.bundle()
-    // log errors if they happen
+////////////////////////////////////////////////////////////////////////////////
+//------------------------- Callable tasks -----------------------------------//
+//----------------------------------------------------------------------------//
+
+// Default task.
+// Builds the website, watches for changes and starts browserSync.
+gulp.task('default', function(done) {
+  shouldWatch = true;
+  runSequence('build', 'watch', 'browser-sync', done);
+});
+
+// Main build task
+gulp.task('build', function(done) {
+  runSequence(['copy_files', 'scripts', 'compass'], done);
+});
+
+// Prod build task
+gulp.task('prod', function(done) {
+  // Change environment.
+  ENV = 'production';
+  runSequence('clean', ['copy_files', 'scripts', 'compass'], done);
+});
+
+// Clean.
+gulp.task('clean', function () {
+  return gulp.src('dist', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('no-reload', function(done) {
+  shouldReload = false;
+  shouldWatch = true;
+  runSequence('build', 'watch', 'browser-sync', done);
+});
+
+
+////////////////////////////////////////////////////////////////////////////////
+//------------------------- Browserify tasks ---------------------------------//
+//--------------------- (Not directly callable) ------------------------------//
+//----------------------------------------------------------------------------//
+
+gulp.task('scripts:build', function() {
+  var b = browserify({
+    entries: ['./app/assets/scripts/main.js'],
+    debug: true,
+    transform: [reactify],
+  })
+    .bundle()
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
     .pipe(source('main.js'))
-    // optional, remove if you don't need to buffer file contents
+    .pipe(buffer());
+
+    // Source maps.
+    if (ENV != 'production') {
+      b = b.pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(sourcemaps.write('./'));
+    }
+
+    if (ENV == 'production') {
+      b = b.pipe(streamify(uglify()));
+    }
+
+    // Output.
+    b.pipe(gulp.dest('./dist/assets/scripts'));
+
+});
+
+gulp.task('scripts:build:watch', function() {
+  var watcher  = watchify(browserify({
+    entries: ['./app/assets/scripts/main.js'],
+    transform: [reactify],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: true
+  }));
+
+  return watcher.on('update', function () {
+    watcher.bundle()
+      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+      .pipe(source('main.js'))
+      .pipe(buffer())
+      // Source maps.
+      .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./dist/assets/scripts'))
+      .on('end', browserReload);
+      console.log('Scripts updated');
+  })
+    .bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('main.js'))
     .pipe(buffer())
-    // optional, remove if you dont want sourcemaps
-    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-       // Add transformation tasks to the pipeline here.
-    .pipe(sourcemaps.write('./')) // writes .map file
+    // Source maps.
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./dist/assets/scripts'))
     .on('end', browserReload);
-}
+});
 
 
+////////////////////////////////////////////////////////////////////////////////
+//--------------------------- Helper tasks -----------------------------------//
+//----------------------------------------------------------------------------//
+
+// I'm watching you!
+gulp.task('watch', function() {
+
+  gulp.watch('app/assets/styles/**/*.scss', function() {
+    runSequence('compass', browserReload);
+  });
+
+  gulp.watch(['app/**/*', '!app/assets/styles/**', '!app/assets/scripts/**'], function() {
+    runSequence('copy_files', browserReload);
+  });
+});
+
+gulp.task('scripts', function(done) {
+  runSequence(shouldWatch ? 'scripts:build:watch' : 'scripts:build', done);
+});
 
 gulp.task('copy_files', function() {
   return gulp.src(['app/**/*', '!app/assets/styles/**', '!app/assets/scripts/**'])
@@ -55,8 +157,8 @@ gulp.task('compass', function() {
     .pipe(compass({
       css: 'dist/assets/styles',
       sass: 'app/assets/styles',
-      style: 'expanded',
-      sourcemap: true,
+      style: ENV = 'production' ? 'compressed' : 'expanded',
+      sourcemap: ENV = 'production' ? false : true,
       bundle_exec: true
     }));
 });
@@ -70,37 +172,6 @@ gulp.task('browser-sync', function() {
   });
 });
 
-// Main build task
-gulp.task('build', function(done) {
-  runSequence(['copy_files', 'scripts', 'compass'], done);
-});
-
-gulp.task('scripts', function(done) {
-  runSequence('scripts:build', done);
-});
-
-// I'm watching you!
-gulp.task('watch', function() {
-  gulp.watch('app/assets/styles/**/*.scss', function() {
-    runSequence('compass', browserReload);
-  });
-
-  gulp.watch(['app/**/*', '!app/assets/styles/**', '!app/assets/scripts/**'], function() {
-    runSequence('copy_files', browserReload);
-  });
-});
-
-// Default task.
-// Builds the website, watches for changes and starts browserSync.
-gulp.task('default', function(done) {
-  runSequence('build', 'watch', 'browser-sync', done);
-});
-
-var shouldReload = true;
-gulp.task('no-reload', function(done) {
-  shouldReload = false;
-  runSequence('build', 'watch', 'browser-sync', done);
-});
 
 ////////////////////////////////////////////////////////////////////////////////
 //------------------------- Helper functions ---------------------------------//

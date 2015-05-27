adac11fe7b39268103f2179aff0d7180072fd8a6
Daniel Silva <daniel@developmentseed.org>
2015-05-27 16:22:12 +0100

    Contribute to #53. Add dropdown system
    Improve copy to clipboard action

---
+++ app/assets/scripts/components/shared/dropdown.js
@@ -0,0 +1,87 @@
+'use strict';
+var React = require('react');
+var Reflux = require('reflux');
+var $ = require('jquery');
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+var dropdownActions = Reflux.createActions({
+  'closeOthers': {}
+});
+
+var activeDropdowns = 0;
+
+var Dropdown = React.createClass({
+  mixins: [Reflux.listenTo(dropdownActions.closeOthers, "onCloseOthers")],
+
+  onCloseOthers: function($exception) {
+    if (this.getDOMNode() != $exception) {
+      this.setState({ open: false });
+    }
+  },
+
+  bodyListener: function(e) {
+    var $clickedDropdown = $(e.target).parents('[data-hook="dropdown"]');
+    dropdownActions.closeOthers($clickedDropdown[0]);
+  },
+
+  getDefaultProps: function() {
+    return {
+      element: 'div',
+      className: '',
+
+      triggerTitle: '',
+      triggerClassName: '',
+      triggerText: '',
+    }
+  },
+
+  getInitialState: function() {
+    return {
+      open: false,
+    }
+  },
+
+  // Lifecycle method.
+  // Called once as soon as the component has a DOM representation.
+  componentDidMount: function() {
+    // With a cross Dropdown variable we ensure that only one event is setup.
+    if (++activeDropdowns === 1) {
+      // Namespace the event so it's easy to remove.
+      $(document).bind('click.dropdown', this.bodyListener);
+    }
+  },
+
+  // Lifecycle method.
+  // Called once as soon as the component has a DOM representation.
+  componentWillUnmount: function() {
+    if (--activeDropdowns === 0) {
+      $(document).unbind('click.dropdown');
+    }
+  },
+
+  closeDropdown: function(e) {
+    e.preventDefault();
+    this.setState({ open: !this.state.open });
+  },
+
+  render: function() {
+    var klasses = ['drop'];
+    if (this.state.open) {
+      klasses.push('open');
+    }
+    if (this.props.className) {
+      klasses.push(this.props.className);
+    }
+
+    return (
+      <this.props.element className={klasses.join(' ')} data-hook="dropdown">
+        <a href="#" title={this.props.triggerTitle} className={this.props.triggerClassName} onClick={this.closeDropdown}><span>{this.props.triggerText}</span></a>
+        <div className="drop-content">
+          {this.props.children}
+        </div>
+      </this.props.element>
+    );
+  }
+});
+
+module.exports = Dropdown;

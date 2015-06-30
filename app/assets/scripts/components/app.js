bf76e4c290af5aba8a90f3d21f8c6e06df144f3b
Anand Thakker <vestibule@anandthakker.net>
2015-06-30 07:46:42 -0400

    Persist search filters in URL

--- app/assets/scripts/components/app.js
+++ app/assets/scripts/components/app.js
@@ -5,8 +5,28 @@ var RouteHandler = Router.RouteHandler;
 var InfoModal = require('./modals/info_modal');
 var WelcomeModal = require('./modals/welcome_modal');
 var Header = require('./header');
+var actions = require('../actions/actions');
 
 var App = React.createClass({
+  mixins: [ Router.State ],
+
+  componentDidMount () {
+    // Pull the search filter state from the URL.  Why is this here instead
+    // of in the Filters component?  Because we want to ensure that we set
+    // these filter parameters BEFORE the map component loads, since that is
+    // where the map move action will get fired, triggering the first API load.
+    //
+    // TODO: this is really a stopgap until we integrate the router more
+    // fully.  (See routes.js for more.)
+    var params = this.getQuery();
+    if (params.date) {
+      actions.setDateFilter(params.date);
+    }
+    if (params.resolution) {
+      actions.setResolutionFilter(params.resolution);
+    }
+  },
+
   render: function() {
     return (
       <div>
--- app/assets/scripts/components/filters.js
+++ app/assets/scripts/components/filters.js
@@ -1,24 +1,48 @@
 'use strict';
 var React = require('react/addons');
 var Reflux = require('reflux');
+var Router = require('react-router');
 var Dropdown = require('./shared/dropdown');
 var actions = require('../actions/actions');
 var searchQuery = require('../stores/search_query_store');
 
 var Filters = module.exports = React.createClass({
   mixins: [
-    Reflux.connect(searchQuery) // calls this.setState() with results of searchQuery store
+    Reflux.connect(searchQuery), // calls this.setState() with results of searchQuery store
+    Router.Navigation,
+    Router.State
   ],
+
+  setDate: function (d) {
+    actions.setDateFilter(d.key);
+    this._updateUrl('date', d.key);
+  },
+
+  setResolution: function (d) {
+    actions.setResolutionFilter(d.key);
+    this._updateUrl('resolution', d.key)
+  },
+
+  _updateUrl: function (prop, value) {
+    var query = this.getQuery();
+    if (value === 'all') {
+      delete query[prop]
+    } else {
+      query[prop] = value;
+    }
+    var routes = this.getRoutes();
+    this.replaceWith(routes[routes.length - 1].name, this.getParams(), query);
+  },
 
   render: function() {
     var dates = [
       {key: 'all', title: 'All'},
       {key: 'week', title: 'Last week'},
-      {key: 'month', title: 'Last week'},
+      {key: 'month', title: 'Last month'},
       {key: 'year', title: 'Last year'}
     ].map(function (d) {
       var klass = this.state.date === d.key ? 'active' : '';
-      var click = actions.setDateFilter.bind(actions, d.key);
+      var click = this.setDate.bind(this, d);
       return (
         <dd key={'date-filter-' + d.key} className={klass}>
           <a onClick={click} title={d.title}>{d.title}</a>
@@ -32,7 +56,7 @@ var Filters = module.exports = React.createClass({
       {key: 'high', title: 'High'}
     ].map(function (d) {
       var klass = this.state.resolution === d.key ? 'active' : '';
-      var click = actions.setResolutionFilter.bind(actions, d.key);
+      var click = this.setResolution.bind(this, d);
       return (
         <dd key={'resolution-filter-' + d.key} className={klass}>
           <a onClick={click} title={d.title}>{d.title}</a>

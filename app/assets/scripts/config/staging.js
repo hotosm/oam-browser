460238b6c689c7fd7eb8eda1424d79abe11c7bcf
Daniel da Silva <daniel@developmentseed.org>
2017-01-25 10:29:50 +0000

    Remove config duplication

--- app/assets/scripts/config/staging.js
+++ app/assets/scripts/config/staging.js
@@ -5,17 +5,6 @@
 
 module.exports = {
   environment: 'staging',
-  map: {
-    mapbox: {
-      accessToken: 'pk.eyJ1IjoiaG90IiwiYSI6ImNpdmlkM2lkMDAwYTAydXBnNXFkd2EwemsifQ.KPrUb_mKlPmHCR6LNrSihQ'
-    },
-
-    initialZoom: 8,
-    minZoom: 2,
-    maxZoom: undefined,
-
-    initialView: [60.177, 25.148]
-  },
   catalog: {
     url: 'https://oam-catalog-staging.herokuapp.com'
   }

b694be533487695c123b76c3fbf6ce5be4dcde35
Nate Smith <nateasmith@gmail.com>
2017-10-18 10:08:30 +0100

    fix lint

--- src/config/production.js
+++ src/config/production.js
@@ -22,5 +22,6 @@ module.exports = {
   oamStatus: "https://status.openaerialmap.org/healthcheck",
   feedbackSubmissionURL:
     "https://getsimpleform.com/messages/ajax?form_api_token=506fc2ac58582416b6086a68a343e344",
-  OAMBrowserUrl: "http://map.openaerialmap.org.s3-website-us-east-1.amazonaws.com/"
+  OAMBrowserUrl:
+    "http://map.openaerialmap.org.s3-website-us-east-1.amazonaws.com/"
 };

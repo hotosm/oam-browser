e13e10d6b272bc65bae35b91353271789ad55792
Nate Smith <nateasmith@gmail.com>
2017-10-18 10:04:04 +0100

    update production config

--- src/config/production.js
+++ src/config/production.js
@@ -2,7 +2,7 @@ module.exports = {
   environment: "production",
   uploadBucket: "oam-uploader-production-temp",
   googleClient:
-    "36015894456-p23fapcrlb8gu5jso00j69uv1c8861j1.apps.googleusercontent.com",
+    "36015894456-cln93odnr88523ssjkaaf8km7fi0snos.apps.googleusercontent.com",
   googleDeveloperKey: "",
   map: {
     mapbox: {
@@ -17,10 +17,10 @@ module.exports = {
     initialView: [-18.632, 18.479]
   },
   catalog: {
-    url: "https://api.openaerialmap.org"
+    url: "http://34.224.18.156"
   },
   oamStatus: "https://status.openaerialmap.org/healthcheck",
   feedbackSubmissionURL:
     "https://getsimpleform.com/messages/ajax?form_api_token=506fc2ac58582416b6086a68a343e344",
-  OAMBrowserUrl: "https://map.openaerialmap.org"
+  OAMBrowserUrl: "http://map.openaerialmap.org.s3-website-us-east-1.amazonaws.com/"
 };

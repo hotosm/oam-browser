7bdd62e9b5cb6d0625625532d6805829d322115b
Nate Smith <nateasmith@gmail.com>
2018-04-30 16:31:31 +0100

    Update staging.js

--- src/config/staging.js
+++ src/config/staging.js
@@ -9,5 +9,6 @@ module.exports = {
   googleClient:
     "36015894456-3d5ka80qtpaqcjhco3lsl38s1fj0dr71.apps.googleusercontent.com",
   googleDeveloperKey: "",
-  OAMBrowserUrl: "http://map-staging.openaerialmap.org/"
+  OAMBrowserUrl: "http://map-staging.openaerialmap.org/",
+  awsKey: "AKIAI4XOYETOVGTNP5HA"
 };

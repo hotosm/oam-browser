a9667b30b50f40eeafb867a43b63f82fddc65213
Dakota Benjamin <dakota.benjamin@hotosm.org>
2020-02-12 12:17:37 -0500

    Add google client auth key to test config

--- src/config/test.js
+++ src/config/test.js
@@ -3,5 +3,8 @@ module.exports = {
     url: "http://localhost:4000"
   },
   OAMBrowserUrl: "http://localhost:3000",
-  uploadBucket: "oam-uploader-staging-temp"
+  uploadBucket: "oam-uploader-staging-temp",
+  googleClient:
+    "36015894456-3d5ka80qtpaqcjhco3lsl38s1fj0dr71.apps.googleusercontent.com",
+  googleDeveloperKey: ""
 };
